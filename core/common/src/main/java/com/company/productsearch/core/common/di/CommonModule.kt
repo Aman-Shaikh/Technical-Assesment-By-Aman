b23f18c69f36674dce1a63fc21d46212814b47b4
Aman Shaikh <85395708+Aman-Shaikh@users.noreply.github.com>
2025-11-17 03:50:52 -0800

    refactor: creates factory for HttpClient

--- core/common/src/main/java/com/company/productsearch/core/common/di/CommonModule.kt
+++ core/common/src/main/java/com/company/productsearch/core/common/di/CommonModule.kt
@@ -2,15 +2,7 @@ package com.company.productsearch.core.common.di
 
 import com.company.productsearch.core.common.config.AppConfig
 import com.company.productsearch.core.common.config.AppConfigImpl
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.okhttp.OkHttp
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.logging.DEFAULT
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logger
-import io.ktor.client.plugins.logging.Logging
-import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.json.Json
+import com.company.productsearch.core.common.network.HttpClientFactory
 import org.koin.dsl.module
 
 // Configuration module
@@ -20,25 +12,6 @@ val configModule = module {
 
 // Network module - Ktor HttpClient configuration
 val networkModule = module {
-    single {
-        HttpClient(OkHttp) {
-            // JSON Content Negotiation
-            install(ContentNegotiation) {
-                json(
-                    Json {
-                        ignoreUnknownKeys = true
-                        prettyPrint = true
-                        isLenient = true
-                        encodeDefaults = false
-                    }
-                )
-            }
-            
-            // Logging (useful for debugging)
-            install(Logging) {
-                logger = Logger.DEFAULT
-                level = LogLevel.ALL
-            }
-        }
-    }
+    single { HttpClientFactory() }
+    single { get<HttpClientFactory>().create() }
 }
